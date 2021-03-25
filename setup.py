df2f4ac90508d03d65f1f3bc2b5e5e64a7fd3412
dustinbleile <dustinbleile@gmail.com>
2021-03-25 15:16:06 -0700

    Version 0.0.1 - setup.py includes basic modules.  gene_scene folder and __init__.py

--- setup.py
+++ setup.py
@@ -1,12 +1,37 @@
+"""Install python requirements and packages.
+
+Suggested to create an isolated virtual environment first:
+    python3 -m venv venv
+
+Activate the environment:
+    source venv/bin/activate
+
+Then make sure pip and setuptools are up to date:
+    python -m pip install -U pip setuptools wheel
+
+Standard installation:
+    python -m pip install -U -e .
+
+Developer installation:
+    python -m pip install -U -e .[dev]
+"""
+
+import codecs
+import os
+
 from setuptools import find_packages, setup
 
-# Dependencies required to use your package
+name = "gene_scene"
+
 INSTALL_REQS = [
     "jupyter",
+    "logzero",
+    "numpy",
+    "pandas",
 ]
 
 # Dependencies required for development
-DEV_REQS = ["flake8", "black", "isort", "mypy", "pylint"]
+DEV_REQS = ["flake8", "black", "isort", "mypy", "pylint", "pydocstyle"]
 
 # Dependencies required only for running tests
 TEST_REQS = ["pytest", "pytest-cov"]
@@ -26,9 +51,27 @@ try:
 except Exception:
     pass
 
+
+def read(rel_path):
+    """Safe read local file."""
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    """Read __version__ property from file."""
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
 setup(
-    name="gene_scene",
-    version="0.0.0",
+    name=name,
+    version=get_version(os.path.join(name, "__init__.py")),
     packages=find_packages(),
     install_requires=INSTALL_REQS,
     extras_require={
